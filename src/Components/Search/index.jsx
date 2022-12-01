097760451e36ef22c57df17bbc08b5d5af0da401
Vladyslav Horbachov <vhorbachov@microsoft.com>
2022-12-01 18:23:26 +0100

    Fixed naming issues

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -8,7 +8,7 @@ const Search = () => {
 
   return (
     <div className={styles.root}>
-      <svg class={styles.icon} viewBox="0 0 32 32">
+      <svg className={styles.icon} viewBox="0 0 32 32">
         <circle
           cx="14"
           cy="14"
@@ -16,19 +16,19 @@ const Search = () => {
           id="XMLID_42_"
           r="9"
           stroke="#000000"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-miterlimit="10"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit={10}
+          strokeWidth="2"
         />
         <line
           fill="none"
           id="XMLID_44_"
           stroke="#000000"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-miterlimit="10"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit={10}
+          strokeWidth="2"
           x1="27"
           x2="20.366"
           y1="27"
